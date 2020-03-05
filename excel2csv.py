d42d6d5ccd181a3b489df19d66da8f4b72460174
Jack YANG <jingkangy91@gmail.com>
2020-03-05 17:22:12 +0800

    Split excel to csv.

--- excel2csv.py
+++ excel2csv.py
@@ -1,2 +1,30 @@
 import os
 import pandas as pd
+
+os.makedirs('csv', exist_ok=True)
+
+input_name = '试验4107list(1).xlsx'
+df = pd.read_excel(input_name)
+
+if input_name.endswith('.xlsx'):
+    print('Correct!')
+
+
+df['group'] = [x // 150 for x in list(df.index)]
+
+
+groups = df.groupby('group')
+
+for i, group in enumerate(groups):
+
+    output_name = os.path.join('csv', input_name.split('.')[0] + str(i).zfill(5) + '.csv')
+    group[1].drop(['group'], axis=1).to_csv(output_name, index=False, encoding='utf-8-sig')
+
+
+
+
+
+
+
+# out_name = 'test.csv'
+# df.to_csv(out_name, index=False, encoding='utf-8-sig')
